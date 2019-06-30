54cf03b0ecb98d14b3feb1f383288887f8db85f0
wsk <1261709167@qq.com>
2019-06-30 10:51:07 +0800

    update

--- src/main/java/com/wsk/controller/ErrorController.java
+++ src/main/java/com/wsk/controller/ErrorController.java
@@ -8,8 +8,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class ErrorController {
+
     @RequestMapping(value = "/error")
     public String error() {
         return "error";
     }
+
 }
